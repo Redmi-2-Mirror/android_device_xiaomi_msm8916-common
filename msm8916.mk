b6b13f75472060d45c33693266ccb15818f78596
dianlujitao <dianlujitao@lineageos.org>
2019-10-12 10:12:37 +0800

    msm8916-common: Enable AOSP bluetooth audio HAL v2 [2/2]
    Change-Id: I4d12752ae5fb2829d6612647c8740c84967f5434

--- msm8916.mk
+++ msm8916.mk
@@ -56,7 +56,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0-service \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
